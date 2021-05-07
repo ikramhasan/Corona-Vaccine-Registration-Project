315ed5e3c2dbf7c8f49b4d17f0c31a3bb600a974
ihni7 <ihnishat1998@gmail.com>
2021-05-07 09:48:15 +0600

    added header in user home screen

--- client/lib/app/presentation/user_home_page.dart
+++ client/lib/app/presentation/user_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:covid_vaccination/app/presentation/components/loading_widget.dart';
 import 'package:covid_vaccination/authentication/data/cubit/user_auth_cubit.dart';
+import 'package:covid_vaccination/authentication/presentation/components/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,9 +10,20 @@ class UserHomePage extends StatelessWidget {
     return BlocBuilder<UserAuthCubit, UserAuthState>(
       builder: (context, state) {
         if (state is UserAuthLoaded) {
-          return LoadingWidget();
+          return Scaffold(
+            body: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 244, vertical: 60),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Header(),
+                ],
+              ),
+            ),
+          );
         }
-        return Container();
+        return LoadingWidget();
       },
     );
   }
