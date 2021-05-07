bbd563017d6bd5661f49b9c659eb6f6341df97b7
ihni7 <ihnishat1998@gmail.com>
2021-05-07 08:40:37 +0600

    added prod config

--- backend/databaseConnection.js
+++ backend/databaseConnection.js
@@ -1,21 +1,20 @@
 const mysql = require("mysql");
 
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "covid vaccination",
+// });
+
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "covid vaccination",
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6409757",
+  password: "3eVEN6Axak",
+  database: "sql6409757",
 });
 
 module.exports = pool;
-
-
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: "sql6.freemysqlhosting.net",
-//   user: "sql6409757",
-//   password: "3eVEN6Axak",
-//   database: "sql6409757",
-// });
