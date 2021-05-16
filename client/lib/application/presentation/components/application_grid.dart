7b67e468e18b0dff80660ff2b80f7e2d5ca0bf4c
ihni7 <ihnishat1998@gmail.com>
2021-05-17 02:01:14 +0600

    added selection of application

--- client/lib/application/presentation/components/application_grid.dart
+++ client/lib/application/presentation/components/application_grid.dart
@@ -1,4 +1,3 @@
-import 'package:covid_vaccination/application/data/models/application.dart';
 import 'package:covid_vaccination/application/data/models/application_list.dart';
 import 'package:flutter/material.dart';
 import 'package:pluto_grid/pluto_grid.dart';
@@ -15,144 +14,189 @@ class ApplicationGrid extends StatefulWidget {
 class _ApplicationGridState extends State<ApplicationGrid> {
   PlutoGridStateManager stateManager;
 
-  // void handleSelected() async {
-  //   String value = '';
+  void handleSelected() async {
+    String value = '';
 
-  //   stateManager.currentSelectingRows.forEach((element) {
-  //     final cellValue = element.cells.entries.first.value.value.toString();
+    stateManager.currentSelectingRows.forEach((element) {
+      final cellValue = element.cells.entries.last.value.value.toString();
 
-  //     value += 'first cell value of row: $cellValue\n';
-  //   });
+      value += 'user id: $cellValue\n';
+    });
 
-  //   if (value.isEmpty) {
-  //     value = 'No rows are selected.';
-  //   }
-  // }
+    if (value.isEmpty) {
+      value = 'No rows are selected.';
+    }
+
+    print(value);
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(widget.applicationList);
     final applicationList = widget.applicationList.data;
-    return Expanded(
-      child: PlutoGrid(
-        onChanged: (PlutoGridOnChangedEvent event) {
-          print(event);
-        },
-        // onLoaded: (PlutoGridOnLoadedEvent event) {
-        //   event.stateManager.setSelectingMode(PlutoGridSelectingMode.row);
+    return Column(
+      children: [
+        Container(
+          height: 500,
+          child: PlutoGrid(
+            onSelected: (event) {
+              print(event.row.toString());
+            },
+            onLoaded: (PlutoGridOnLoadedEvent event) {
+              event.stateManager.setSelectingMode(PlutoGridSelectingMode.row);
 
-        //   stateManager = event.stateManager;
-        // },
-        columns: [
-          PlutoColumn(
-            width: 80,
-            title: 'ID',
-            field: 'number_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.number(readOnly: true),
-          ),
-          PlutoColumn(
-            title: 'Name',
-            field: 'name_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.text(readOnly: true),
-          ),
-          PlutoColumn(
-            width: 80,
-            title: 'Age',
-            field: 'age_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.number(readOnly: true),
-          ),
-          PlutoColumn(
-            title: 'Occupation',
-            field: 'occupation_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.text(readOnly: true),
-          ),
-          PlutoColumn(
-            title: 'Location',
-            field: 'location_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.text(readOnly: true),
-          ),
-          PlutoColumn(
-            title: 'NID',
-            field: 'nid_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.number(readOnly: true),
-          ),
-          PlutoColumn(
-            title: 'Email',
-            field: 'email_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.text(readOnly: true),
-          ),
-          PlutoColumn(
-            title: 'Phone No',
-            field: 'phone_field',
-            enableEditingMode: false,
-            type: PlutoColumnType.text(readOnly: true),
-          ),
-          PlutoColumn(
-            title: 'Date',
-            field: 'date_field',
-            type: PlutoColumnType.date(),
-          ),
-          PlutoColumn(
-            title: 'Vaccination Center',
-            field: 'text_field',
-            type: PlutoColumnType.text(),
-          ),
-          PlutoColumn(
-            title: 'Comment',
-            field: 'admin_comment',
-            type: PlutoColumnType.text(),
-          ),
-        ],
-        rows: List.generate(
-          widget.applicationList.data.length,
-          (index) {
-            return PlutoRow(
-              cells: {
-                'number_field': PlutoCell(
-                  value: applicationList[index].application.applicationId,
-                ),
-                'name_field': PlutoCell(
-                  value: applicationList[index].application.name,
-                ),
-                'age_field': PlutoCell(
-                  value: applicationList[index].application.age,
-                ),
-                'occupation_field': PlutoCell(
-                  value: applicationList[index].application.occupation,
-                ),
-                'location_field': PlutoCell(
-                  value: applicationList[index].application.location,
-                ),
-                'nid_field': PlutoCell(
-                  value: applicationList[index].application.nid,
-                ),
-                'email_field': PlutoCell(
-                  value: applicationList[index].application.email,
-                ),
-                'phone_field': PlutoCell(
-                  value: applicationList[index].application.phoneNo,
-                ),
-                'date_field': PlutoCell(
-                  value: applicationList[index].application.submissionDate,
-                ),
-                'text_field': PlutoCell(
-                  value: applicationList[index].application.vaccinationCenter,
-                ),
-                'admin_comment': PlutoCell(
-                  value: applicationList[index].application.adminComment,
-                ),
+              stateManager = event.stateManager;
+            },
+            onChanged: (PlutoGridOnChangedEvent event) {
+              print(event);
+            },
+            // onLoaded: (PlutoGridOnLoadedEvent event) {
+            //   event.stateManager.setSelectingMode(PlutoGridSelectingMode.row);
+
+            //   stateManager = event.stateManager;
+            // },
+            columns: [
+              PlutoColumn(
+                width: 80,
+                title: 'ID',
+                field: 'number_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.number(readOnly: true),
+              ),
+              PlutoColumn(
+                title: 'Name',
+                field: 'name_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.text(readOnly: true),
+              ),
+              PlutoColumn(
+                width: 80,
+                title: 'Age',
+                field: 'age_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.number(readOnly: true),
+              ),
+              PlutoColumn(
+                title: 'Occupation',
+                field: 'occupation_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.text(readOnly: true),
+              ),
+              PlutoColumn(
+                title: 'Location',
+                field: 'location_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.text(readOnly: true),
+              ),
+              PlutoColumn(
+                title: 'NID',
+                field: 'nid_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.number(readOnly: true),
+              ),
+              PlutoColumn(
+                title: 'Email',
+                field: 'email_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.text(readOnly: true),
+              ),
+              PlutoColumn(
+                title: 'Phone No',
+                field: 'phone_field',
+                enableEditingMode: false,
+                type: PlutoColumnType.text(readOnly: true),
+              ),
+              PlutoColumn(
+                title: 'Date',
+                field: 'date_field',
+                type: PlutoColumnType.date(),
+              ),
+              PlutoColumn(
+                title: 'Vaccination Center',
+                field: 'text_field',
+                type: PlutoColumnType.text(),
+              ),
+              PlutoColumn(
+                title: 'Comment',
+                field: 'admin_comment',
+                type: PlutoColumnType.text(),
+              ),
+              PlutoColumn(
+                title: 'Dose 1 Status',
+                field: 'dose_stat_1',
+                type: PlutoColumnType.text(),
+                frozen: PlutoColumnFrozen.right,
+              ),
+              PlutoColumn(
+                title: 'Dose 2 Status',
+                field: 'dose_stat_2',
+                type: PlutoColumnType.text(),
+                frozen: PlutoColumnFrozen.right,
+              ),
+              PlutoColumn(
+                title: 'User id',
+                field: 'user_id',
+                type: PlutoColumnType.number(),
+              ),
+            ],
+            rows: List.generate(
+              widget.applicationList.data.length,
+              (index) {
+                return PlutoRow(
+                  cells: {
+                    'number_field': PlutoCell(
+                      value: applicationList[index].application.applicationId,
+                    ),
+                    'name_field': PlutoCell(
+                      value: applicationList[index].application.name,
+                    ),
+                    'age_field': PlutoCell(
+                      value: applicationList[index].application.age,
+                    ),
+                    'occupation_field': PlutoCell(
+                      value: applicationList[index].application.occupation,
+                    ),
+                    'location_field': PlutoCell(
+                      value: applicationList[index].application.location,
+                    ),
+                    'nid_field': PlutoCell(
+                      value: applicationList[index].application.nid,
+                    ),
+                    'email_field': PlutoCell(
+                      value: applicationList[index].application.email,
+                    ),
+                    'phone_field': PlutoCell(
+                      value: applicationList[index].application.phoneNo,
+                    ),
+                    'date_field': PlutoCell(
+                      value: applicationList[index].application.submissionDate,
+                    ),
+                    'text_field': PlutoCell(
+                      value:
+                          applicationList[index].application.vaccinationCenter,
+                    ),
+                    'admin_comment': PlutoCell(
+                      value: applicationList[index].application.adminComment,
+                    ),
+                    'dose_stat_1': PlutoCell(
+                      value: applicationList[index].doseList.isEmpty
+                          ? 'Pending'
+                          : applicationList[index].doseList.first.status,
+                    ),
+                    'dose_stat_2': PlutoCell(
+                      value: applicationList[index].doseList.isEmpty
+                          ? 'Pending'
+                          : applicationList[index].doseList.last.status,
+                    ),
+                    'user_id': PlutoCell(
+                      value: applicationList[index].application.userId,
+                    ),
+                  },
+                );
               },
-            );
-          },
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
